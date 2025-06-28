ef22b426934fb7a499bab7f3d566dd6669097649
Sistemas-IA <ihermanowyc@gmail.com>
2025-06-28 09:26:53 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('registrationForm').addEventListener('submit', async function (e) {
-  e.preventDefault();
-  // Validaciones y envío aquí
-});
-
-function closeModal() {
-  document.getElementById('modal').classList.add('hidden');
-  document.getElementById('registrationForm').reset();
-}
