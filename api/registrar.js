869c7cab341b1ace3a3fd6e4134ee58c9a7e4709
Sistemas-IA <ihermanowyc@gmail.com>
2025-06-28 15:26:17 -0300

    Add files via upload

---
+++ api/registrar.js
@@ -0,0 +1,21 @@
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ success: false, message: "Método no permitido" });
+  }
+
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbzLALjHRxSx-8YG9XM8uIBZkCOSeESZSxZ5AAjb1oAN_4Ji_o7KAXnu9EtSxh2uA2Fc9Q/exec", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(req.body),
+    });
+
+    const result = await response.json();
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Error al conectar con GAS" });
+  }
+}
