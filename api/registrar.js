a14366108e90f89d815e5298f581187189484acd
Sistemas-IA <ihermanowyc@gmail.com>
2025-07-01 21:23:57 -0300

    Update registrar.js

--- api/registrar.js
+++ api/registrar.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbwxV7XSToS9PpyeESpA2qP1StyhGQiY2Pdlz8yYITjM50KeogrIacQgHaQiubdjNskN/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbw9iz_vkca6qT8NzKeHybVM0HvSYjs5B1AmqJpfB3xdciYsghA6yWKPo78MySb0Z6asOA/exec", {
       method: "POST",
       body: JSON.stringify(datos),
       headers: {
