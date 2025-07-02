382471f0045862793d993678162997e9af9a7cee
Sistemas-IA <ihermanowyc@gmail.com>
2025-07-01 21:22:19 -0300

    Add files via upload

---
+++ api/registrar.js
@@ -0,0 +1,28 @@
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send("Método no permitido");
+  }
+
+  const datos = req.body;
+
+  // Filtro básico por seguridad
+  if (typeof datos !== "object" || !datos.nombre || !datos.dni) {
+    return res.status(400).send("Solicitud inválida");
+  }
+
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbwxV7XSToS9PpyeESpA2qP1StyhGQiY2Pdlz8yYITjM50KeogrIacQgHaQiubdjNskN/exec", {
+      method: "POST",
+      body: JSON.stringify(datos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const texto = await response.text();
+    res.status(200).send(texto);
+  } catch (error) {
+    res.status(500).send("Error al conectar con el servidor");
+  }
+}
