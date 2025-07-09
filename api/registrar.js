b5acda7bfb7dee831e44019ba81feaeb712a7634
Sistemas-IA <ihermanowyc@gmail.com>
2025-07-09 14:48:42 -0300

    Update registrar.js

--- api/registrar.js
+++ api/registrar.js
@@ -1,7 +1,8 @@
 // api/registrar.js  (reenviando el token al backend GAS)
 // --- Pon tus valores aquí ---
 const API_URL = "https://script.google.com/macros/s/AKfycbyhOmXxYWg8qz1PtZmtzkRtOmA5PdA8cNXGL11b8D8nNw17o746GT4lVDvYgiexy3CDWA/exec";
-const API_KEY = "6LdNAXUrAAAAAPIQkyTVFPqs1S0a6Eb2oczOnsG8";   // la misma que tienes en GAS
+const API_KEY = "c4c164f9-f6ab-4b29-a8b6-91394cf17e27
+";   // la misma que tienes en GAS
 // ----------------------------
 
 export default async function handler(req, res) {
