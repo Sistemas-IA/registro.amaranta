2686da15e0dfaeff4990ccdfc58c7d4f4a5e3039
Sistemas-IA <ihermanowyc@gmail.com>
2025-09-09 09:03:36 -0300

    Update register.js

--- api/register.js
+++ api/register.js
@@ -98,7 +98,7 @@ export default async function handler(req, res) {
       telefono, sanitize(email),
       sanitize(direccion.slice(0,100)),
       sanitize(comentarios.slice(0,250)),
-      'Z-0','Pendiente',
+      'Z-00','Pendiente',
       sanitize(lista.slice(0,50)),
       new Date().toISOString(), ip
     ];
